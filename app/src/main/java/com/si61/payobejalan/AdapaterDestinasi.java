ba42fba225dc8c6892b9fd296a3ae5da56315e91
Felix Kennedy <felixkennedy00@gmail.com>
2023-03-30 17:23:06 +0700

    Commit 7

--- app/src/main/java/com/si61/payobejalan/AdapaterDestinasi.java
+++ app/src/main/java/com/si61/payobejalan/AdapaterDestinasi.java
@@ -1,6 +1,8 @@
 package com.si61.payobejalan;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +15,11 @@ import java.util.ArrayList;
 
 public class AdapaterDestinasi extends RecyclerView.Adapter<AdapaterDestinasi.ViewHolderDestinasi> {
     private Context ctx;
-    private ArrayList arrNama, arrAlamat, arrJam;
+    private ArrayList arrNama, arrAlamat, arrJam, arrId;
 
-    public AdapaterDestinasi(Context ctx, ArrayList arrNama, ArrayList arrAlamat, ArrayList arrJam) {
+    public AdapaterDestinasi(Context ctx, ArrayList arrId, ArrayList arrNama, ArrayList arrAlamat, ArrayList arrJam) {
         this.ctx = ctx;
+        this.arrId = arrId;
         this.arrNama = arrNama;
         this.arrAlamat = arrAlamat;
         this.arrJam = arrJam;
@@ -31,6 +34,7 @@ public class AdapaterDestinasi extends RecyclerView.Adapter<AdapaterDestinasi.Vi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderDestinasi holder, int position) {
+        holder.tvId.setText(arrId.get(position).toString());
         holder.tvNama.setText(arrNama.get(position).toString());
         holder.tvAlamat.setText(arrAlamat.get(position).toString());
         holder.tvJam.setText(arrJam.get(position).toString());
@@ -42,12 +46,42 @@ public class AdapaterDestinasi extends RecyclerView.Adapter<AdapaterDestinasi.Vi
     }
 
     public class ViewHolderDestinasi extends RecyclerView.ViewHolder {
-        TextView tvNama, tvAlamat, tvJam;
+        TextView tvId, tvNama, tvAlamat, tvJam;
+
         public ViewHolderDestinasi(@NonNull View itemView) {
             super(itemView);
+            tvId = itemView.findViewById(R.id.tv_Id);
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvAlamat = itemView.findViewById(R.id.tv_alamat);
             tvJam = itemView.findViewById(R.id.tv_jam);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Perintah Apa yang Akan Dilakukan?");
+                    pesan.setCancelable(true);
+
+                    pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+
+                    });
+
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    pesan.show();
+                    return false;
+                }
+            });
         }
     }
 }
